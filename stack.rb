3badd25101a5441a2dfd70d51cf8b458edba226d
[01;31m[K"name"[m[K: "Woojae Yi <[01;31m[K"email"[m[K: null>
2017-05-06 19:46:37 -0700

    Implement and write tests for Stack#pop, Stack#top, Stack#length using a FixedArray data structure

--- stack.rb
+++ stack.rb
@@ -4,6 +4,9 @@ class Stack
 
   attr_reader :count, :stack
 
+  class StackOutOfBounds < StandardError
+  end
+
   def initialize
     @stack = FixedArray.new(3)
     @count = 0
@@ -20,6 +23,29 @@ class Stack
     else
       false
     end
+  end
+
+  def pop
+    if !empty?
+      @count -= 1
+      toReturn = @stack.get(@count)
+      @stack.set(@count, nil)
+      toReturn
+    else
+      raise StackOutOfBounds, "Error: Stack is empty"
+    end
+  end
+
+  def top
+    if !empty?
+      @stack.get(@count-1)
+    else
+      raise StackOutOfBounds, "Error: Stack is empty"
+    end
+  end
+
+  def length
+    @count
   end
 
 
